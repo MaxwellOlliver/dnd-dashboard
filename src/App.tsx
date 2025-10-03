5f0ca94afdca15f7bd787ffb6501dc69d111169d
Maxwell Macedo <maxwellmacedo2015@gmail.com>
2025-10-03 00:27:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import { DndPanel } from "@/features/dnd-panel/pages/dnd-panel";
+
+function App() {
+  return <DndPanel />;
+}
+
+export default App;
---
+++ src/features/dnd-panel/pages/dnd-panel.tsx
@@ -0,0 +1,3 @@
+export const DndPanel = () => {
+  return <div>DndPanel</div>;
+};
